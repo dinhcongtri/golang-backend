a32472ea67ca9a1930e5e7a6058537cd48e10bc4
DinhCongTri <tri@ancient8.gg>
2023-07-05 14:17:24 +0700

    feat: add route

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	prisma "golang-prisma/db"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
+	"strconv"
 )
 
 func HashPassword(password string) (string, error) {
@@ -30,8 +30,16 @@ type Login struct {
 	Password string `json:"password"`
 }
 
+type CreateNote struct {
+	UserId   int    `json:"userId"`
+	Content  string `json:"content"`
+	CreateAt string `json:"createAt"`
+	UpdateAt string `json:"updateAt"`
+}
+
 func main() {
 	client := prisma.NewClient()
+
 	errPrisma := client.Prisma.Connect()
 	if errPrisma != nil {
 		return
@@ -46,6 +54,7 @@ func main() {
 	ctx := context.Background()
 
 	router := gin.Default()
+	//===============================REST API=====================================
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
@@ -71,14 +80,12 @@ func main() {
 			return
 		}
 
-		data, err := client.User.CreateOne(prisma.User.Username.Set(signup.Username), prisma.User.Password.Set(hashedPassword)).Exec(ctx)
+		data, errCreate := client.User.CreateOne(prisma.User.Username.Set(signup.Username), prisma.User.Password.Set(hashedPassword)).Exec(ctx)
 
-		if err != nil {
+		if errCreate != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-
-		fmt.Println(data)
 
 		c.JSON(http.StatusCreated, gin.H{
 			"message": "User created",
@@ -106,6 +113,54 @@ func main() {
 			})
 			return
 		}
+	})
+
+	router.POST("/create-note", func(c *gin.Context) {
+		var createNote CreateNote
+		if err := c.BindJSON(&createNote); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		data, errCreate := client.Note.CreateOne(prisma.Note.Content.Set(createNote.Content)).Exec(ctx)
+		if errCreate != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errCreate.Error()})
+			return
+		}
+
+		dataRoot, errRoot := client.UserNote.CreateOne(prisma.UserNote.UserID.Cursor(createNote.UserId), prisma.UserNote.NoteID.Cursor(data.ID)).Exec(ctx)
+		if errRoot != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errRoot.Error()})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{
+			"message": "Note created",
+			"data":    dataRoot,
+		})
+		return
+	})
+
+	router.GET("/note", func(c *gin.Context) {
+		param := c.Request.URL.Query()
+		noteId := param["id"][0]
+		userId, _ := strconv.ParseInt(c.Request.Header.Get("userId"), 10, 0)
+
+		_, errFind := client.UserNote.FindMany(prisma.UserNote.NoteID.Equals(noteId), prisma.UserNote.UserID.Equals(int(userId))).Exec(ctx)
+		if errFind != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errFind.Error()})
+			return
+		}
+
+		data, errNote := client.Note.FindUnique(prisma.Note.ID.Equals(noteId)).Exec(ctx)
+		if errNote != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": errNote.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
 	})
 
 	errRouter := router.Run("localhost:8080")
